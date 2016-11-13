534471d4c37fb553c0edfebb6fe7962808c2031c
Nico Pelsmaekers <nico.pelsmaekers@student.kdg.be>
2016-11-13 16:31:40 +0100

    website KV Embedded Systems

--- assets/js/iframe.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#mijnEventIframe").load(function() {
-        var doc = this.contentDocument || this.contentWindow.document;
-        var target = doc.getElementById("content");
-        target.innerHTML = "Found It!"; 
-    });
-});
- 
